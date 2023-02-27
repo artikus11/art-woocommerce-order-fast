ce2148bcbce4a68b8faf0f7623e3aeb0e5c6d444
Артем <artikus.sol@gmail.com>
2023-02-27 17:19:24 +0400

    UPD 1.2.0: изменеие отключения хуков

--- classes/class-front.php
+++ classes/class-front.php
@@ -7,13 +7,19 @@ class Front {
 	public function init(): void {
 
 		if ( 'replace' === get_option( 'woocommerce_awof_mode' ) ) {
-			remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
-			remove_action( 'woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20 );
+			add_action( 'after_setup_theme', [ $this, 'remove' ] );
 		}
 
 		add_action( 'woocommerce_proceed_to_checkout', [ $this, 'render' ], 30 );
 		add_action( 'woocommerce_widget_shopping_cart_after_buttons', [ $this, 'render' ], 30 );
 
+	}
+
+
+	public function remove(): void {
+
+		remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+		remove_action( 'woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20 );
 	}
 
 
